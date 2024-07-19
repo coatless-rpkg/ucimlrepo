50b5d8d4e2f69730292b633e27da2c749a2b6eb7
James J Balamuta <james.balamuta@gmail.com>
2024-07-19 15:08:03 -0700

    Add tests for list_available_dataset()

---
+++ tests/testthat/test-list-available-datasets.R
@@ -0,0 +1,32 @@
+## Test list_available_datasets() function ----
+
+test_that("list_available_datasets(): graceful errors", {
+  skip_on_cran()
+
+  # Test that the function displays a diagnostic message instead of a hard error.
+  expect_message(list_available_datasets(area = 'health and medicin'))
+  expect_message(list_available_datasets(filter = 'abc'))
+  expect_message(list_available_datasets(search = ''))
+
+})
+
+test_that("list_available_datasets(): search", {
+  skip_on_cran()
+
+  # Check that the function outputs text
+  expect_output(df <- list_available_datasets(area = 'climate and environment'))
+
+  # Check dataframe is present
+  expect_s3_class(df, 'data.frame')
+
+  # Check names of data frame output
+  expect_equal(names(df), c('id', 'name', 'url'))
+
+  # Check that the function outputs text when filter and area are set
+  expect_output(list_available_datasets(filter = 'python', area = 'climate and environment'))
+
+  # Check that the function outputs text when search and area are set
+  expect_output(list_available_datasets(search = 'nino', area = 'climate and environment'))
+
+})
+
